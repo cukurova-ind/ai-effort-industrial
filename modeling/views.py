6544c39400385b1c7797d9f2a457cbfc1a6cebad
cukurova-ind <cukurovaindustrialprojects@gmail.com>
2024-08-15 16:15:26 +0300

    data form

--- modeling/views.py
+++ modeling/views.py
@@ -10,6 +10,7 @@ from django.conf import settings
 from django.db.models import Q
 from dataops.models import Experiment, Input
 from .image_processor import Preprocessor
+from .utils import util
 
 def main_page(req):
 
@@ -63,5 +64,40 @@ def image_settings(req):
                     {"i": {"total": total, "output": output, "raw": raw, "hypo": hypo, "err": err}})
     
 def data_settings(req):
+    image_training_path = os.path.join(settings.MEDIA_ROOT, "modeling", "image", "training")
+    image_input_path = os.path.join(image_training_path, "input")
+    image_target_path = os.path.join(image_training_path, "target")
+    csv_training_path = os.path.join(settings.MEDIA_ROOT, "modeling", "csv", "training")
+    csv_input_path = os.path.join(csv_training_path, "input")
+    csv_target_path = os.path.join(csv_training_path, "target")
+    if not os.path.exists(image_training_path):   
+        os.makedirs(image_input_path)
+        os.makedirs(image_target_path)
+    if not os.path.exists(csv_training_path):
+        os.makedirs(csv_input_path)
+        os.makedirs(csv_target_path)
 
-    return render(req, "data_process.html")
+    image_extensions = ('.png', '.jpg', '.jpeg')
+    image_input_num, image_target_num = 0, 0
+    for root, dirs, files in os.walk(image_training_path):
+        for file in files:
+            if file.lower().endswith(image_extensions):
+                folder_name = root.split("/")[-1]
+                if folder_name=="input":
+                    image_input_num += 1
+                if folder_name=="target":
+                    image_target_num += 1 
+
+    if req.method == "POST":
+        postdata = req.POST
+        print([util.get(p) for p in postdata])
+        return render(req, "data_process.html")
+    
+    if req.method == "GET":
+        return render(req, "data_process.html",
+                       {"iip": image_input_path,
+                        "iin": image_input_num,
+                        "itn": image_target_num,
+                        "itp": image_target_path,
+                        "cip": csv_input_path,
+                        "ctp": csv_target_path})
